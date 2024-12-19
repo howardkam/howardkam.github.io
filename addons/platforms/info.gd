c92fedc4b957f637074c8415c53acca07c10ebc2
Howard Kam <16825191+howardkam@users.noreply.github.com>
2024-12-19 13:43:34 -1000

    COmmit

--- addons/platforms/info.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-## Remember to open "Editable Children"!
-
-@export_multiline var README = "Right click and enable editable children!" ## Important to do this if you want to use properly[br]Enable Editable Children!
